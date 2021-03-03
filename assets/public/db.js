5f2cb689d9b8447ff55c7bb254c7d476b596be7a
Bryan Moreno <bfourbryan@gmail.com>
2021-03-03 15:36:59 -0800

    Addition of saveRecord and checkDatabase functions.

--- assets/public/db.js
+++ assets/public/db.js
@@ -20,3 +20,45 @@ request.onsuccess = function(event) {
 request.onerror = function(event) {
     console.log("Something has gone wrong! " + event.target.errorCode);
 };
+
+
+function saveRecord(record) {
+
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    store.add (record);
+
+}
+
+function checkDatabase() {
+
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    const getAll = store.getAll();
+
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+
+                const store = transaction.objectStore("pending");
+
+                store.clear();
+            });
+        }
+    };
+}
